f865cde1da8c6bafbcd770dacf816974d111b7ae
circlindev-caleb <caleb@circlin.co.kr>
2022-06-30 21:45:59 +0900

    Testing board upload(POST api)

--- global_configuration/helper.py
+++ global_configuration/helper.py
@@ -238,11 +238,11 @@ def video_to_mp4(path):
     else:
         pass
 
-    new_path = f"./temp/{path.split('/')[-1].split('.')[0]}.mp4"
+    new_path = f"./temp/{path.split('/')[-1].split('.')[0]}_{width}_{height}.mp4"
     os.system(f"ffmpeg -i {path} -vf scale={width}x{height} {new_path}")
 
-    if os.path.exists(path):
-        os.remove(path)
+    # if os.path.exists(path):
+    #     os.remove(path)
 
     return new_path
 
