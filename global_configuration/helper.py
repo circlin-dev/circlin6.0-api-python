a1d31e2ddc71caaf6202736b8121af68f14f7514
circlindev-caleb <caleb@circlin.co.kr>
2022-07-01 19:25:14 +0900

    Fixing directory error..

--- global_configuration/helper.py
+++ global_configuration/helper.py
@@ -118,13 +118,9 @@ def upload_single_file_to_s3(file, object_path):
     hashed_file_name = f"{hashlib.sha256(original_file_name.split('.')[0].encode()).hexdigest()}_{random_string}.{original_file_name.split('.')[1]}"
     # hashed_file = os.path.join('./temp', hashed_file_name)
     # hashed_file = os.path.join(APP_TEMP, hashed_file_name)
-    hashed_file = os.path.join(os.path.join(os.getcwd(), 'temp'), hashed_file_name)
+    hashed_file = os.path.join(os.getcwd(), 'temp', hashed_file_name)
     os.rename(original_file, hashed_file)
-    # try:
-    #     shutil.move(original_file, hashed_file)
-    # except Exception as e:
-    #     error = {'result': f"Error: ({e}) {request_path}, {original_file}, {original_file_name}, {hashed_file} || current dir: {os.getcwd()}"}
-    #     return error
+    # shutil.move(original_file, hashed_file)
 
     hashed_object_name = os.path.join(object_path, hashed_file_name)
     hashed_mime_type = check_mimetype(hashed_file)['mime_type']  # Insert to DB
@@ -244,11 +240,12 @@ def video_to_mp4(path):
     else:
         pass
 
-    new_path = os.path.join(os.getcwd(), 'temp', f"{path.split('/')[-1].split('.')[0]}.mp4")
+    # new_path = os.path.join(os.getcwd(), 'temp', f"{path.split('/')[-1].split('.')[0]}.mp4")
+    new_path = os.path.join(os.getcwd(), 'temp', f"testnewvideo.mp4")
     os.system(f"ffmpeg -i {path} -vf scale={width}x{height} {new_path}")
     # subprocess.Popen(["ffmpeg", "-i", path, f"scale={width}x{height}", new_path])
-    if os.path.exists(path):
-        os.remove(path)
+    # if os.path.exists(path):
+    #     os.remove(path)
 
     return new_path
 
