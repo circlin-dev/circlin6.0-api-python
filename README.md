c543f7a07474ebaafb9db39effc4f5b0e6771a4b
circlindev-caleb <caleb@circlin.co.kr>
2022-10-05 17:02:42 +0900

    .gitignore 적용중

--- README.md
+++
@@ -1,2 +0,0 @@
-# circlin6.0-api-python
-써클인 앱(2022.6.29. 현재 6.0 버전) python api server
--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_cors import CORS
-
-api = Blueprint('api', __name__)
-CORS(api, supports_credentials=True)
-
-
-from . import board
