5820116c78ceb548b6557be52b8c1457d0ab2cd5
circlindev-caleb <caleb@circlin.co.kr>
2023-01-03 11:34:13 +0900

    챌린지 랭킹 변수명 변경 및 단위표시 보완

--- adapter/repository/mission_rank.py
+++ adapter/repository/mission_rank.py
@@ -1,4 +1,4 @@
-from adapter.orm import follows, users
+from adapter.orm import follows, mission_playgrounds, users
 from domain.mission import MissionRank, MissionRankUser
 from domain.user import User
 
@@ -21,6 +21,10 @@ class AbstractMissionRankRepository(abc.ABC):
 
     @abc.abstractmethod
     def get_my_rank(self, mission_rank_id: int, user_id: int):
+        pass
+
+    @abc.abstractmethod
+    def get_rank_scale(self, mission_id: int):
         pass
 
 
@@ -81,6 +85,7 @@ class MissionRankRepository(AbstractMissionRankRepository):
         return self.session.execute(sql).scalar()
 
     def get_my_rank(self, mission_rank_id: int, user_id: int):
+        follower_count = select(func.count(follows.c.id)).where(follows.c.target_id == users.c.id)
         sql = select(
             users.c.id.label('user_id'),
             users.c.nickname,
@@ -89,13 +94,15 @@ class MissionRankRepository(AbstractMissionRankRepository):
             MissionRankUser.rank,
             MissionRankUser.feeds_count,
             MissionRankUser.summation,
-            func.concat(func.lpad(MissionRankUser.rank, 15, '0')).label('cursor'),
+            # select(mission_playgrounds.c.rank_scale).where(mission_playgrounds.c.mission_id == mission_id).label("scale"),
+            follower_count.label('followers'),
         ).join(
             users, MissionRankUser.user_id == users.c.id
         ).where(
             and_(
                 MissionRankUser.mission_rank_id == mission_rank_id,
-                MissionRankUser.user_id == user_id,
+                # MissionRankUser.user_id == user_id,
+                users.c.id == user_id,
                 users.c.deleted_at == None,
             )
         ).order_by(
@@ -104,3 +111,7 @@ class MissionRankRepository(AbstractMissionRankRepository):
 
         result = self.session.execute(sql).first()
         return result
+
+    def get_rank_scale(self, mission_id: int):
+        sql = select(mission_playgrounds.c.rank_scale).where(mission_playgrounds.c.mission_id == mission_id)
+        return self.session.execute(sql).first()
